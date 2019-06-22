490c67389a769b1d25ae31fc89c8415360b5dd53
mattias17 <kisquian@gmail.com>
2019-06-21 22:45:58 -0300

    about info++

---
+++ precache-manifest.5188e2601c371b078ff7902b4d32dd2f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efcdcbb9c4462ae4e3d13586657aee0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1a80cf7c9362f34f0fd3",
+    "url": "/static/js/main.33d64c23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
